f6e483dc280c679f968ed7471b43b9b2d3b6f2c2
Andreas Maus <maus@badphish.ypbind.de>
2020-05-16 17:15:32 +0200

    check_shelf_state: Check state of a shelf

---
+++ cmd/check_shelf_state/constants.go
@@ -0,0 +1,32 @@
+package main
+
+const name = "check_shelf_state"
+const helpText = `Usage %s --host=<host> --user=<user> --password=<pwd>|--password-file=<pwdf>
+         [--ca-file=<file>] [--help] [--insecure] [--timeout=<sec>] [--version]
+
+  --ca-file=<file>          Use CA certificate from <file> for validation of SSL certificate
+
+  --help                    This text
+
+  --host=<host>             Host to connect to
+
+  --insecure                Skip validation of SSL server certificate
+
+  --password=<pwd>          Password for authentication
+
+  --password-file=<pwdf>    Read password from password file
+                            Note: Only the first line of the file is used
+
+  --user=<user>             Username for authentication
+
+  --timeout=<sec>           HTTP connection timeout in seconds
+                            Default: %d sec.
+
+  --version                 Shows version
+
+  --volume=<vol>            Only check volume <vol>
+                            Default: check all volumes
+
+`
+
+const defaultHTTPTimeout = 60
